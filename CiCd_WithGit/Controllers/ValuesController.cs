3477bc421eb7f5a5b5df5fec7d6c97733b6a755b
VedikaGujar <vgujar@getnerdio.com>
2020-06-01 09:47:39 +0530

    init

---
+++ CiCd_WithGit/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CiCd_WithGit.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController()
+        {
+        }
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "TestCiCd";
+        }
+
+
+        //[HttpGet("/value")]
+        //public ActionResult<string> GetValue()
+        //{
+        //    return "TestCiCd";
+        //}
+    }
+}
---
+++ NunitTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+using CiCd_WithGit.Controllers;
+using NUnit.Framework;
+
+namespace NunitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        ValuesController controller = new ValuesController();
+        [Test]
+        public void GetReturnsCorrectNumber()
+        {
+            var returnValue = controller.Get(1);
+            Assert.AreEqual("TestCiCd", returnValue.Value);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
